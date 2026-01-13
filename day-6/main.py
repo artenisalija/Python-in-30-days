eb6f5063a05debe6d69057c92b7e36399ed928be
artenisalija <artenisalija@gmail.com>
2026-01-13 20:01:27 +0100

    Fixed main.py to fix CI issues

--- day-6/main.py
+++ day-6/main.py
@@ -1,8 +1,15 @@
+import sys
 from core.logic import process_request
 from utils.helpers import format_result, error_message
 
+
 def main():
-    user_input = input("Enter a number: ")
+    # If a value was passed from the command line (CI, Docker, etc)
+    if len(sys.argv) > 1:
+        user_input = sys.argv[1]
+    else:
+        # Otherwise, allow interactive input (local use)
+        user_input = input("Enter a number: ")
 
     try:
         number = int(user_input)
