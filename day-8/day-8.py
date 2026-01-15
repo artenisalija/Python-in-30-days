521a48f4b6d9d5f7bfb891eb58d40dcab2c40b4b
artenisalija <artenisalija@gmail.com>
2026-01-15 21:44:18 +0100

    Day-8 Logging Module and Initial Code

---
+++ day-8/day-8.py
@@ -0,0 +1,24 @@
+import logging
+
+logging.basicConfig(filename='day-8.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+class Dishes:
+    def __init__(self, breakfast, lunch, dinner):
+        self.breakfast = breakfast
+        self.lunch = lunch
+        self.dinner = dinner
+        logging.info("Dishes instance created with breakfast: %s, lunch: %s, dinner: %s", breakfast, lunch, dinner)
+
+    def total_calories(self):
+        total = self.breakfast + self.lunch + self.dinner
+        logging.info("Total calories calculated: %d", total)
+        return total
+    
+day_1_dishes = Dishes(500, 700, 800)
+day_2_dishes = Dishes(600, 600, 900)
+day_3_dishes = Dishes(400, 800, 700)
+
+print("Day 1 Total Calories:", day_1_dishes.total_calories())
+print("Day 2 Total Calories:", day_2_dishes.total_calories())
+print("Day 3 Total Calories:", day_3_dishes.total_calories())
+        
