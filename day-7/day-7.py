cf2e08d4ce38b7cacab096a3016752a5436c7358
artenisalija <artenisalija@gmail.com>
2026-01-15 21:54:20 +0100

    fixed typo in day-7/day-7.py

--- day-7/day-7.py
+++ day-7/day-7.py
@@ -83,7 +83,7 @@ for task in tasks:
 data = [asdict(task) for task in tasks]
 
 # Save back to JSON
-with open("day7/tasks.json", "w") as f:
+with open("day-7/tasks.json", "w") as f:
     json.dump(data, f, indent=2)
 
 print("✅ Tasks updated and saved.")
